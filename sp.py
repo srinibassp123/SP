85de01ed92bb4a1ed98dda5153a3fd7f9e03eb89
srinibassp123 <157303896+srinibassp123@users.noreply.github.com>
2024-03-15 10:35:48 +0530

    Update sp.py\

--- sp.py
+++ sp.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return '21BCS7837'
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
